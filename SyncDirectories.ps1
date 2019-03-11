10e950509ec9475957a6ea3f8cb717e4878085cd
Morgan Lyddon <lyddon.morg@gmail.com>
2019-03-10 20:46:16 -0500

    Added References

--- SyncDirectories.ps1
+++ SyncDirectories.ps1
@@ -1,3 +1,9 @@
+#References
+#https://www.adamtheautomator.com/how-to-compare-the-contents-of-two-folders/
+#https://www.business.com/articles/powershell-sync-folders/
+#Robocopy
+  #robocopy {source} {destination} /xc /xn /xo /s
+
 $LocalPlexPath = 'E:\Plex\Media\TV Shows'
 $ExternalPlexPath = 'F:\Plex\Media\TV Shows'
 
